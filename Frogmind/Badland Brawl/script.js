189ca4af3bb2a55d8e3b86d97452c8f8381e6e39
Kitenok 228 <75540213+Kitten-EASY@users.noreply.github.com>
2022-03-05 00:29:51 +0300

    gene?
    ogo new scripts upload

---
+++ Frogmind/Badland Brawl/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let cache = {
+	modules: {},
+	options: {}
+};
+
+let base = Process.findModuleByName('libbadlandbrawl.so').base;
+let ntohs = new NativeFunction(Module.findExportByName('libc.so', 'ntohs'), 'uint16', ['uint16']);
+let inet_addr = new NativeFunction(Module.findExportByName('libc.so', 'inet_addr'), 'int', ['pointer']);
+
+function setup() {
+	Interceptor.attach(Module.findExportByName('libc.so', 'getaddrinfo'), {
+		onEnter: function(args) {
+			let connecthost = args[0].readUtf8String();
+			console.log(connecthost);
+			if (connecthost === 'game.badlandapi.com') {
+				let newhost = cache.options.redirectHost;
+				args[0].writeUtf8String(newhost);
+			}
+		}
+	});
+}
+
+// startup
+rpc.exports = {
+	init: function(stage, options) {
+		cache.options = options || {};
+		cache.base = Process.findModuleByName('libbadlandbrawl.so').base;
+		cache.options.redirectHost = "game.stage.rumblestars.com";
+		
+		setup();
+		//Interceptor.detachAll();
+	}
+};
+// command to run listen: frida -U Gadget -l script.js --no-pause --runtime=v8
